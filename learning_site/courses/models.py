afba566b7883e819d74c285767794b63405d04c2
John Orenza <johnorenza@gmail.com>
2017-09-02 09:35:43 -0700

    Added a str method to the class.

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -8,3 +8,6 @@ class Course(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
