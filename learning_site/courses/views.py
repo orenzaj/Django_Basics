d85aa2ce4cd7d27d152b2ef654e9945d33931687
John Orenza <johnorenza@gmail.com>
2017-09-02 17:02:32 -0700

    A view that will list all courses

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# This will display a list of all the courses.
+def course_list(request):
+    courses = Course.objects.all()      # query all courses in Course model
+    output = ',\n'.join(course for course in courses)   # make a string with given queryset
+    return HttpResponse(output)
