ce1a3b3178d45ebf7e2fbc537f9e805742b4faa2
John Orenza <johnorenza@gmail.com>
2017-09-02 18:11:07 -0700

    Added another course through the admin panel.

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+# First import your model
+from .models import Course
+
 # Register your models here.
+admin.site.register(Course)
