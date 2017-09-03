ff3efc70c275094da0e8520a9769e82afbbf36fb
John Orenza <johnorenza@gmail.com>
2017-09-02 18:27:28 -0700

    Rendered output to some fancy HTML

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render     # 3 args:
+                                        #   request (same parameter as view)
+                                        #   template_path
+                                        #   context (dictionary)
 
 from .models import Course
 # Create your views here.
 # This will display a list of all the courses.
 def course_list(request):
     courses = Course.objects.all()      # query all courses in Course model
-    output = ',\n'.join(str(course) for course in courses)   # make a string with given queryset
-    return HttpResponse(output)
+    return render(request, 'courses/course_list.html', {'courses': courses})
