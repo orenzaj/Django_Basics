a204811abde26feaf7fa620f721902ddce2aeb77
John Orenza <johnorenza@gmail.com>
2017-09-02 17:43:28 -0700

    Fixed minor output error.

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Course
 # Create your views here.
 # This will display a list of all the courses.
 def course_list(request):
     courses = Course.objects.all()      # query all courses in Course model
-    output = ',\n'.join(course for course in courses)   # make a string with given queryset
+    output = ',\n'.join(str(course) for course in courses)   # make a string with given queryset
     return HttpResponse(output)
