d61c26e1aafd1630262ad5ac8366fb35b370d58e
John Orenza <johnorenza@gmail.com>
2017-09-04 20:29:40 -0700

    Added StepInline class to enable multi-editing

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -3,9 +3,18 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# First import your model
+# First import your models
 from .models import Course
+from .models import Step
+
+
+# These classes will enable editing multiple steps within the course.
+class StepInline(admin.StackedInline):
+    model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInline,]
 
 # Register your models here.
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
