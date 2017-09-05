e9e99cf292f1ae8aadc872d9bf7df05a11371419
John Orenza <johnorenza@gmail.com>
2017-09-04 21:13:10 -0700

    Added a course detail view that takes in a primary key

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -3,13 +3,20 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render     # 3 args:
-                                        #   request (same parameter as view)
-                                        #   template_path
-                                        #   context (dictionary)
+#   request (same parameter as view)
+#   template_path
+#   context (dictionary)
 
 from .models import Course
 # Create your views here.
 # This will display a list of all the courses.
+
+
 def course_list(request):
     courses = Course.objects.all()      # query all courses in Course model
     return render(request, 'courses/course_list.html', {'courses': courses})
+
+
+def course_detail(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
