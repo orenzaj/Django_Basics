d37d701ada0902f00af7780cd060677bc4430dee
John Orenza <johnorenza@gmail.com>
2017-09-04 21:23:07 -0700

    Added a Meta class in the Step model to implement ordering.

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models        # Need this import for models
 
 # Models are conventionally singular and will always inherit from models.Model
+
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,6 +20,9 @@ class Step(models.Model):
     description = models.TextField()
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
+
+    class Meta:
+        ordering = ['order', ]
 
     def __str__(self):
         return self.title
