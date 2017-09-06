bd4d9f4a039f4cd330f5111453e8c65de3a77dce
John Orenza <johnorenza@gmail.com>
2017-09-05 19:10:45 -0700

    Fixed missing parameters.

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, get_object_or_404
 #   template_path
 #   context (dictionary)
 
-from .models import Course
+from .models import Course, Step
 # Create your views here.
 # This will display a list of all the courses.
 
@@ -22,3 +22,8 @@ def course_detail(request, pk):
     #course = Course.objects.get(pk=pk)
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step': step})
